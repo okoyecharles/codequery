0623477416191f072097e79fa3e16c772fbdbdca
Charles <okoyecharles509@gmail.com>
2025-01-04 10:24:24 +0100

    ref: remove default files

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home.component';
+import { SearchComponent } from './features/search/search.component';
+import { QueryComponent } from './features/query/query.component';
+import { ErrorComponent } from './shared/components/error/error.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'search',
+    component: SearchComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'query/:id',
+    component: QueryComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    component: ErrorComponent,
+    pathMatch: 'full',
+  }
+];
