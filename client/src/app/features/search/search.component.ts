801fcbed85b253f575450001348bfd63f5bde476
Charles <okoyecharles509@gmail.com>
2025-01-04 14:24:04 +0100

    feat: add search layout

--- client/src/app/features/search/search.component.ts
+++ client/src/app/features/search/search.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './search.component.html',
   styleUrl: './search.component.scss'
 })
